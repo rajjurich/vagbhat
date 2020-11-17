a68b0d7154d74aa96c206a3f1b57cff6977820f3
Rajneesh <rajju2512@gmail.com>
2020-11-17 10:46:06 +0530

    ok

---
+++ vagbhat.web/Pages/IndexBase.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace vagbhat.web.Pages
+{
+    public class IndexBase : ComponentBase
+    {
+        protected string VisibilityText { get; set; }
+        [Inject]
+        NavigationManager NavigationManager { get; set; }
+        protected void NewPatient()
+        {
+            NavigationManager.NavigateTo("/addpatient");
+        }
+
+        protected override Task OnInitializedAsync()
+        {
+            return Task.FromResult(VisibilityText = "show");
+        }
+
+        protected void ToggleVisibility()
+        {
+            VisibilityText = VisibilityText == "show" ? "hide" : "show";
+        }
+    }
+}
---
+++ vagbhat.web/Shared/VisitorBase.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace vagbhat.web.Shared
+{
+    public class VisitorBase : ComponentBase
+    {
+        
+        //public string DivCssClass { get; set; }
+        [Parameter]
+        public string Visible { get; set; } = "show";
+        public string DivCssClass => Visible == "show" ? "collapse" : null;
+    }
+}
